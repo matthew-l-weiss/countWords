fbe6a718ffc96d0f8ca087993e061fa02820976d
weissml <matthew.weiss@dematic.com>
2022-09-07 08:33:07 -0400

    More succinct

--- src/main/java/wordcount/App.java
+++ src/main/java/wordcount/App.java
@@ -1,7 +1,6 @@
 package wordcount;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
@@ -9,27 +8,24 @@ import java.util.Scanner;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+public class App {
 
-public class App 
-{
-   public void countWords(String sentence) {
-
-      Map<Long, List<String>> groupedOccurrences = Arrays.asList(sentence.split(" ")).stream()
-            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream()
-            .collect(Collectors.groupingBy(Map.Entry::getValue, Collectors.mapping(Map.Entry::getKey, Collectors.toList())));
-
-      for (Long key : groupedOccurrences.keySet().stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList())) {
-         Collection<String> collection = groupedOccurrences.get(key);
-         for (String occurrence : collection) {
-            System.out.println(key + " " + occurrence);
-         }
-      }
+   public List<Map.Entry<Long, List<String>>> countWords(String sentence) {
+      return Arrays.asList(sentence.split(" ")).stream()
+         .collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream()
+         .collect(Collectors.groupingBy(Map.Entry::getValue, Collectors.mapping(Map.Entry::getKey, Collectors.toList())))
+         .entrySet().stream().sorted(Map.Entry.comparingByKey(Comparator.reverseOrder())).collect(Collectors.toList());
    }
 
    public static void main(String[] args) {
       System.out.println("Enter a string and press enter");
       Scanner in = new Scanner(System.in);
       App instance = new App();
-      instance.countWords(in.nextLine());
+      List<Map.Entry<Long, List<String>>> wordCount = instance.countWords(in.nextLine());
+      for (Map.Entry<Long, List<String>> e : wordCount) {
+         for (String word : e.getValue()) {
+            System.out.println(e.getKey() + " " + word);
+         }
+      }
    }
 }
