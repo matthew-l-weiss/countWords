6a177f1b1d723d34fe98bb5e841c391ccf28ca7c
weissml <matthew.weiss@dematic.com>
2022-09-07 00:05:40 -0400

    Cleanup

--- src/main/java/wordcount/App.java
+++ src/main/java/wordcount/App.java
@@ -1,34 +1,27 @@
 package wordcount;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.SortedMap;
-import java.util.TreeMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 
 public class App 
 {
-
    public void countWords(String sentence) {
-      SortedMap<Long, Collection<String>> groupedOccurrences = new TreeMap<>(Collections.reverseOrder());
 
-      Arrays.asList(sentence.split(" ")).stream()
-            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet()
-            .forEach((entry) -> {
-               Collection<String> stringList = groupedOccurrences.getOrDefault(entry.getValue(), new ArrayList<>());
-               stringList.add(entry.getKey());
-               groupedOccurrences.put(entry.getValue(), stringList);
-            });
+      Map<Long, List<String>> groupedOccurrences = Arrays.asList(sentence.split(" ")).stream()
+            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream()
+            .collect(Collectors.groupingBy(Map.Entry::getValue, Collectors.mapping(Map.Entry::getKey, Collectors.toList())));
 
-      for (Long wordCount : groupedOccurrences.keySet()) {
-         Collection<String> words = groupedOccurrences.get(wordCount);
-         for (String word : words) {
-            System.out.println(wordCount + " " + word);
+      for (Long key : groupedOccurrences.keySet().stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList())) {
+         Collection<String> collection = groupedOccurrences.get(key);
+         for (String occurrence : collection) {
+            System.out.println(key + " " + occurrence);
          }
       }
    }
